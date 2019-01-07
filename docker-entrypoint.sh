ba3b74a475f84ed96cb798f324b2f4ea1ffd4909
Akhil Nayabu <akhilnayabu@gmail.com>
2019-01-07 01:25:46 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 touch /config/application.properties
 
-if [-d /run/secrets/ ]; then
+if [  -d /run/secrets/ ]; then
     for filename in /run/secrets/*; do
         echo "${filename##*/}=`cat $filename`" >> /config/application.properties
     done
